13ce025b7f9d119ad79721911d48143c576063b3
kimminseok <bjm7701@naver.com>
2023-10-08 19:25:02 +0900

    first commit

---
+++ src/main/java/com/example/housingfinanceloanrate/HousingFinanceLoanRateApplication.java
@@ -0,0 +1,13 @@
+package com.example.housingfinanceloanrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingFinanceLoanRateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousingFinanceLoanRateApplication.class, args);
+    }
+
+}
