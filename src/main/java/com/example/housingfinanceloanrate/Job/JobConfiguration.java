fc8cbba2d151b762ab54dac81fdb5d35b2392b35
kimminseok <bjm7701@naver.com>
2023-10-13 11:03:08 +0900

    datasource 삭제

--- src/main/java/com/example/housingfinanceloanrate/Job/JobConfiguration.java
+++ src/main/java/com/example/housingfinanceloanrate/Job/JobConfiguration.java
@@ -5,16 +5,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobScope;
-import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
@@ -23,10 +18,8 @@ import javax.sql.DataSource;
 @Slf4j
 @RequiredArgsConstructor
 public class JobConfiguration {
-    @Qualifier("secondDataSource")
     private final DataSource dataSource;
     private final RentLoanRateInfoTasklet rentLoanRateInfoTasklet;
-    @Qualifier("secondPlatformTransactionManager")
     private final PlatformTransactionManager platformTransactionManager;
     private final JobRepository jobRepository;
     @Bean
