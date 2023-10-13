bb580c0f756a53e91727d451ec7ad1e9629eb767
kimminseok <bjm7701@naver.com>
2023-10-13 11:09:42 +0900

    JobLauncher 만듬

--- src/main/java/com/example/housingfinanceloanrate/Job/JobConfiguration.java
+++ src/main/java/com/example/housingfinanceloanrate/Job/JobConfiguration.java
@@ -3,16 +3,19 @@ package com.example.housingfinanceloanrate.Job;
 import com.example.housingfinanceloanrate.Tasklet.RentLoanRateInfoTasklet;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.TaskExecutorJobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.time.LocalDateTime;
 
 @Configuration
 @Slf4j
@@ -22,12 +25,28 @@ public class JobConfiguration {
     private final RentLoanRateInfoTasklet rentLoanRateInfoTasklet;
     private final PlatformTransactionManager platformTransactionManager;
     private final JobRepository jobRepository;
-    @Bean
-    public Job rentLoanRateInfoJob() {
 
-        return new JobBuilder("RentLoanRateInfoJob",jobRepository)
-                .start(rentLoanRateInfoStep())
-                .build();
+    @Bean
+    public JobParameters jobParameters() {
+        return new JobParametersBuilder()
+                .addLocalDateTime("executeDate", LocalDateTime.now())
+                .toJobParameters();
+    }
+    @Bean
+    public JobExecution jobExecution() throws Exception {
+        return jobLauncher().run(
+                new JobBuilder("RentLoanRateInfoJob",jobRepository)
+                        .start(rentLoanRateInfoStep())
+                        .build()
+                ,jobParameters());
+    }
+    @Bean
+    public JobLauncher jobLauncher() throws Exception {
+        TaskExecutorJobLauncher taskExecutorJobLauncher = new TaskExecutorJobLauncher();
+        taskExecutorJobLauncher.setJobRepository(jobRepository);
+        taskExecutorJobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        taskExecutorJobLauncher.afterPropertiesSet();
+        return taskExecutorJobLauncher;
     }
     @Bean
     public Step rentLoanRateInfoStep() {
